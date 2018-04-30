0562cc39042f21695a845f623e00be99bd1e119f
nandkumarj <nandkumarj@vmware.com>
2018-04-30 06:01:22 -0400

    update REST API to cover more HTTP status codes #51

--- Gen 3/mwc-nfv-hackathon/backend/restwebservice.py
+++ Gen 3/mwc-nfv-hackathon/backend/restwebservice.py
@@ -27,6 +27,7 @@ from flask import Flask, render_template, send_from_directory
 from flask_cors import CORS, cross_origin
 from werkzeug.datastructures import ImmutableMultiDict
 
+
 from rest_generate_blueprint import rest_create_blueprint_package, rest_cleanup,rest_gen_name_and_workdir
 from database import db_check_credentials,db_user_signup,db_generate_newpassword,db_update_user_with_token,db_delete_token_from_user,db_check_token
 import logging
@@ -35,7 +36,7 @@ from logging.handlers import RotatingFileHandler
 from werkzeug.utils import secure_filename
 from sendemail import sendMail,draft_mail_text
 from flask_jwt import JWT, jwt_required, current_identity
-from  catalog import _getVIMs,_getOrchsforVIM,_getInputHeads,_getInputHeadDetails
+from  catalog import _getVIMs,_getOrchsforVIM,_getInputHeads,_getInputHeadDetails,_getVNFTypes
 
 from prefixmiddleware import PrefixMiddleware
 from flask import Flask, request
@@ -49,11 +50,10 @@ import jwt
 import datetime
 import os
 import pprint
+from flask import Response
 app = Flask(__name__)
 app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='/api')
 
-#jwt = JWT(app)
-api = Api(app)
 app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
 app.config['UPLOAD_FOLDER'] = './uploads'
 app.config['REST_ROUTE_V1'] = '/api/v1'
@@ -64,19 +64,64 @@ app.config['APPLICATION_ROOT'] = '/backend/app'
 def init():
   return jsonify({"Message" : "Welcome"})
 
+def check_contentType(f):
+    @wraps(f)
+    def decorated(*args,**kwargs):
+        print "check_contentType:",request.headers
+        if 'Content-Type' in request.headers:
+            contype = request.headers['Content-Type']
+            if contype != 'application/json':
+               return jsonify({"Error" : 'Bad Request. Content-Type should be "application/json"'}),415
+        else:
+           return jsonify({'Error' : 'No Content-Type in header' }),400
+
+        if 'Accept' in request.headers:
+           accepttype = request.headers['Accept']
+           if accepttype != 'application/json':
+              return jsonify({"Error" : "Bad Request. API won't generate Client's peferred media type"}),406
+        return f(*args,**kwargs)
+    return decorated
+
+
+def check_contentType_forblueprintapi(f):
+    @wraps(f)
+    def decorated(*args,**kwargs):
+        print "check_contentType:",request.headers
+        if 'Content-Type' in request.headers:
+            contype = request.headers['Content-Type']
+            print "check_contentyType_forblueprintapi",contype
+            if 'multipart/form-data' not in contype:
+               return jsonify({"Error" : 'Bad Request. Content-Type should be "multipart/form-data"'}),415
+        else:
+           return jsonify({'Error' : 'No Content-Type in header' }),400
+
+        if 'Accept' in request.headers:
+           accepttype = request.headers['Accept']
+           if accepttype != 'application/zip':
+              return jsonify({"Error" : "Bad Request. API won't generate Client's peferred media type"}),406
+        return f(*args,**kwargs)
+    return decorated
+
+
+
 @app.route('/v1/Login',methods=['POST'])
 
 @cross_origin(origin='*')
 
+@check_contentType
 def login():
   if request.method == 'POST':
+     print "request:",request
      print "Received Login request:",request.data
   if request.data == "":
-     return jsonify({"Error":"User Credentials not provided",'status': '200'})
+     return jsonify({'Error':'Empty Request Body','Content-Type':'application/json'}),400
+     #return jsonify({'Error':'Empty Request Body','status':'400'})
+     #return Response("{'Error':'Empty Request Body'}",status = 400, mimetype='application/json')
 
   auth = json.loads(request.data)
-  if not auth or not auth['username'] or not auth['password']:
-      return jsonify({'Message':'Bad Request or UserName or Password Missing in request data','status': '200'})
+  if not 'username' in auth.keys() or not 'password' in auth.keys():
+      #return jsonify({'Error':'Bad Request or UserName or Password Missing in request data','status': '400'})
+      return jsonify({'Error':'Bad Request or UserName or Password Missing in request data'}),400
   credentials = auth
   print(credentials['username'] ,credentials['password'])
   if db_check_credentials(credentials['username'] ,credentials['password']):
@@ -85,42 +130,40 @@ def login():
         #return jsonify({'token': token.decode('UTF-8')})
         print "login:",token
         db_update_user_with_token(credentials['username'],token)
-        return jsonify({'token': token,'status':'200'})
-  return jsonify({'Message' : "Authentication Failed",'status':'200'})
-
+        return jsonify({'token': token}),200
+  return jsonify({'Error' : "Authentication Failed. Invalid Username or password"}),200
 
 
 
 @app.route('/v1/RegisterUser',methods=['POST'])
-
+@check_contentType
 def RegisterUser():
   if request.method == 'POST':
      print "Received User Registration Request"
      print "Register User:received signup request"
+     credentials = {}
      if request.data:
         print "Request Data:",request.data
         #credentials = request.data
         credentials = json.loads(request.data)
         
         print(credentials['username'],credentials['password'])
-     elif request.form:
-        credentials = request.form
-        print "Register User:",credentials
 
-
+        if 'username' not in credentials.keys() or 'emailaddress' not in credentials.keys() or 'password' not in credentials.keys() or 'confirmpassword' not in credentials.keys():
+            return jsonify({"Error": "Incorrect parameters in request body"}),400
      status = db_user_signup(credentials['username'],credentials['password'],credentials['emailaddress'])
      print(status)
      if(status == "True"):
         mail_text = draft_mail_text("User Registration",credentials['username'],credentials['password'])
         print "signup:",mail_text
         sendMail([credentials['emailaddress']],"Rest Service VNF Onboarding User Registration",mail_text)
-        return jsonify({"Success": "Registraton Succeeded for User {}".format(credentials['username']),"status" : "200"})  
+        return jsonify({"Success": "Registraton Succeeded for User {}".format(credentials['username'])}),'200'  
      else:
-        return jsonify({'Failure':"User or Email ID exists", "status": "200"}) 
+        return jsonify({'Error':"Registration Failed. User or Email ID exists"}), 409 
 
 
 @app.route('/v1/forgetpassword', methods=['POST'])
-
+@check_contentType
 def forgetpassword():
    if request.method == 'POST':
       if request.data:
@@ -147,7 +190,7 @@ def forgetpassword():
       else:
            return jsonify({"Error": 'Bad Request. Request payload empty','status':'400'})
 
-
+   
 
 def token_required(f):
     @wraps(f)
@@ -158,18 +201,18 @@ def token_required(f):
 	   token = request.headers['x-access-token']
            print "token_required",token
         if not token:
-           return jsonify({"Error": "token is missing",'status':'200'})
+           return jsonify({"Error": "token is missing"}),400
         try:
            data = jwt.decode(token,app.config['JWT_SECRET_KEY'],'UTF-8')
            print "token_rquired",data
            currentUser = data['username']
            print "Received token for User:",currentUser
         except:
-	   return jsonify({"message" : "invalid token or token expired"})
+	   return jsonify({'Error' : 'invalid token or token expired'}),401
 
 
         if db_check_token(currentUser) != token:   
-           return jsonify({"Error" : "User is Logged out"})
+           return jsonify({'Error' : 'User is Logged out.No longer authorized to perform operation'}),403
 
         return f(currentUser,*args,**kwargs)
     return decorated           
@@ -183,24 +226,29 @@ def protected(currentUser):
 
 
 @app.route('/v1/VIMS',methods=['GET'])
+@check_contentType
 @token_required
 def listvims(currentUser):
    if request.method == 'GET':
       return jsonify({"Success":"List of Supported VIMS","VIMS":_getVIMs()})
 
 @app.route('/v1/Orchestrators' ,methods = ['GET'])
+@check_contentType
 @token_required
 def getOrchs(currentUser):
    if request.method == 'GET':   
       vim = request.args.get('vim')
-      print "getOrchs: vim:",vim
+      print "getOrchs:vim:",vim
+      if vim is None:
+        return jsonify({"Error": "Expected argument vim not provided"}),400
       if vim in _getVIMs():
-        return jsonify({"Success" : "List of Supported Orchestrators", "Orchestrators" : _getOrchsforVIM(vim),'status': '200'})
+        return jsonify({"Success" : "List of Supported Orchestrators", "Orchestrators" : _getOrchsforVIM(vim)}),200
       else:
-        return jsonify({"Failure": "Unsupported VIM {}".format(vim),'status':'200'})
+        return jsonify({"Failure": "Unsupported VIM {}".format(vim)}),400
 
 
 @app.route('/v1/VNF Types' ,methods = ['GET'])
+@check_contentType
 @token_required
 def listVNFTypes(currentUser):
    if request.method == 'GET':
@@ -208,25 +256,29 @@ def listVNFTypes(currentUser):
       orch = request.args.get('orch')
       print "getOrchs: vim:",vim
       if vim in _getVIMs() and orch in _getOrchsforVIM(vim):
-         return jsonify({"Success" : "List of Supported VNFs", "VNF Types" : _getOrchsforVIM(vim),'status': '200'})
+         return jsonify({"Success" : "List of Supported VNFs", "VNF Types" : _getVNFTypes(vim,orch)}),200
       else:
-         return jsonify({"Failure": "Unsupported VIM {}".format(vim),'status':'200'})
+         return jsonify({"Error": "Unsupported VIM {} or Orchestrator {}".format(vim,orch)}),400
 
 
 
 @app.route('/v1/InputHeads',methods = ['GET'])
+@check_contentType
 @token_required
 def getInputHeads(currentUser):
     if request.method == 'GET':
        vim = request.args.get('vim')
        orch = request.args.get('orch')
+       if vim is None or orch is None:
+          return jsonify({"Error" : "Required arguments (vim/orch) missing"}),400  
        print "getInputHeads: vim:{},Orch:{}".format(vim,orch)
        if vim in _getVIMs() and orch in _getOrchsforVIM(vim):
-          return jsonify({"Success" : "Retrieved Input Heads", "InputHeads":"InputHeads for vim :{} and Orch:{} are:{}".format(vim,orch,_getInputHeads(vim,orch))})
+          return jsonify({"Success" : "Retrieved InputHeads for vim:{} and Orch:{}".format(vim,orch), "InputHeads":_getInputHeads(vim,orch)}),200
        else :
-          return jsonify({"Failure" : "VIM or Orchestrator not supported",'status':'200'})
+          return jsonify({"Error" : "VIM :{} or Orchestrator:{} not supported".format(vim,orch)}),400
 
 @app.route( '/v1/InputHeadDetails',methods = ['GET'])
+@check_contentType
 @token_required
 def getInputHeadDetails(currentUser):
     if request.method == 'GET':
@@ -234,25 +286,25 @@ def getInputHeadDetails(currentUser):
        orch = request.args.get('orch')
        inputHead = request.args.get('inputHead')
        print "getInputHeadDetails: vim:{},Orch:{},inputHead:{}".format(vim,orch,inputHead)
-       if vim in _getVIMs():
-         print "vim present",vim
+       if vim is None:
+         return jsonify({"Error" : "Required argument vim not present"}),400
 
-       if orch in _getOrchsforVIM(vim):
-         print "Orch present",orch
+       if orch is None:
+         return jsonify({"Error" : "Required argument orch(Orchestrator) not present"}),400
       
-       if inputHead in _getInputHeads(vim,orch):
-          print "inputHead present",inputHead
-       else:
-          print "not present",inputHead,_getInputHeads(vim,orch)
+       if inputHead is None:
+          return jsonify({"Error" : "Required argument inputHead  not present"}),400
+
        if vim in _getVIMs() and orch in _getOrchsforVIM(vim) and inputHead in _getInputHeads(vim,orch):
             return jsonify({"Success" : "Retrieved Details for InputHead:{} for {} + {} combination ".format(inputHead,vim,orch), "Data": _getInputHeadDetails(vim,orch,inputHead)})
        else:
-            return jsonify({"Failure": "Could not find {} Details for {} + {}".format(inputHead,vim,orch)})
+            return jsonify({"Error": "Could not find {} Details for {} + {}".format(inputHead,vim,orch)}),400
 
 
 
 
 @app.route('/v1/blueprint',methods=['POST'])
+@check_contentType_forblueprintapi
 @token_required
 def upload(currentUser):
    if request.method == 'POST':
@@ -294,7 +346,7 @@ def upload(currentUser):
                 uploaded_files.append(script_name)
                 inputs['params']['scripts']['create'] = script_name
               else:
-	        return jsonify({"Error":"cloud_init_script not provided"})
+	        return jsonify({"Error":"cloud_init_script not provided"}),400
          else:
            if 'create' in request.files.keys():
              create_script = request.files['create']
@@ -318,7 +370,7 @@ def upload(currentUser):
              inputs['params']['scripts']['delete'] = delete_script_name
        
            if not uploaded_files:
-              return ({"Error" : "File upload Failed"})
+              return ({"Error" : "File upload Failed"}),500
 
    print "Received: ",inputs
    output_file, workdir = rest_create_blueprint_package(inputs)
@@ -328,15 +380,16 @@ def upload(currentUser):
                    as_attachment=True,
                    attachment_filename=os.path.basename(output_file))
    rest_cleanup(os.path.dirname(workdir))
-   return resp
+   return resp,200
 
 
 
 @app.route('/v1/Logout',methods=['POST'])
+@check_contentType
 @token_required
 def logout(currentUser):
     if db_delete_token_from_user(currentUser) == True:
-       return jsonify({"Success" : "user {} is logged out".format(currentUser)})
+       return jsonify({"Success" : "user {} is logged out".format(currentUser)}),200
 
   
 
